b7e5a0b4192df93232b5c1599f0ed129876609fa
Muhammad Shehroz Sarmad <shehrozm107@gmail.com>
2025-06-14 23:24:43 +0500

    refactor: update nodemailer transport configuration to use mailtrap

--- src/services/nodemailer.service.js
+++ src/services/nodemailer.service.js
@@ -1,25 +1,35 @@
 import nodemailer from "nodemailer";
 import { ApiError } from "../utils/apiError.js";
 
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: process.env.NODEMAILER_EMAIL,
+//         pass: process.env.NODEMAILER_PASS,
+//     },
+// });
+
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // true for 465, false for other ports
     auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PASS,
+        user: process.env.FROM_EMAIL_ADDRESS,
+        pass: process.env.PASSWORD,
     },
 });
 
 const sendEmail = async (email, subject, message) => {
     try {
         const res = await transporter.sendMail({
-            from: "Byte2Bite",
+            from: `${process.env.FROM} <${process.env.FROM_EMAIL_ADDRESS}>`,
             to: email,
             subject,
             html: message,
         });
-        console.log("email sent");
+        console.log("email sent", res);
         return res;
     } catch (error) {
         console.error("Error sending OTP via email:", error);
