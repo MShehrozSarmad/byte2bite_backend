619443b97c13f1fcd0a7b37d3009c520e593737f
Muhammad Shehroz Sarmad <shehrozm107@gmail.com>
2025-06-26 13:53:32 +0500

    refactor: update nodemailer transport configuration to use environment variables

--- src/services/nodemailer.service.js
+++ src/services/nodemailer.service.js
@@ -1,25 +1,24 @@
 import nodemailer from "nodemailer";
 import { ApiError } from "../utils/apiError.js";
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PASS,
-    },
-});
-
 // const transporter = nodemailer.createTransport({
-//     host: process.env.SMTP_HOST,
-//     port: process.env.SMTP_PORT,
-//     secure: false, // true for 465, false for other ports
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
 //     auth: {
-//         user: process.env.FROM_EMAIL_ADDRESS,
-//         pass: process.env.PASSWORD,
+//         user: process.env.NODEMAILER_EMAIL,
+//         pass: process.env.NODEMAILER_PASS,
 //     },
 // });
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
+    },
+});
 
 const sendEmail = async (email, subject, message) => {
     try {
